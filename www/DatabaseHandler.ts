0db707e6d50cef006bcd13e6f02b34acdd3efbd6
Devin <demitchell14@gmail.com>
2018-11-17 18:22:12 -0600

    Mongo Database interactions

---
+++ www/DatabaseHandler.ts
@@ -0,0 +1,101 @@
+import * as mongo from "mongodb";
+import {MongoDetails} from "../config"
+import {Collection, Cursor, FilterQuery, MongoClient, ObjectID, UpdateQuery} from "mongodb";
+
+export class Database {
+
+    readonly client:Promise<MongoClient>;
+    private collection:Collection;
+    private _collection:string;
+    private autoTimeout:any;
+    private timeout:number;
+    public constructor(opts?:any) {
+        this.client = mongo.MongoClient.connect(url(), {
+            useNewUrlParser: true
+        });
+        this.timeout = 5 * 1000; // 60 seconds
+        this.resetTimeout();
+    }
+    public async openCollection(collection:string) {
+        this.resetTimeout();
+        const client = await this.client;
+
+        this.collection = client.db(MongoDetails.db).collection(collection)
+        this._collection = collection;
+        return this;
+    }
+    public find(query:FilterQuery<UserObject|SessionObject>):Cursor<UserObject|SessionObject> {
+        this.resetTimeout();
+        const col = this.collection;
+        return col.find(query);
+    }
+
+    public async update(query:FilterQuery<UserObject|SessionObject>, update:UpdateQuery<UserObject|SessionObject>) {
+        this.resetTimeout();
+        const col = this.collection;
+
+
+        return await col.updateOne(query, update)
+    }
+
+    public async insert(query:Partial<UserObject|SessionObject>) {
+        this.resetTimeout();
+        const col = this.collection;
+        let base;
+        switch (this._collection) {
+            case "sessions":
+                base = {
+                    sessionToken: "",
+                    expiration: 0,
+                    userToken: ""
+                };
+                break;
+            default:
+                base = {};
+        }
+
+        return col.insertOne(Object.assign(base, query));
+    }
+
+    public close() {
+        clearTimeout(this.autoTimeout);
+        this.client.then(c => c.close());
+    }
+
+    private resetTimeout() {
+        clearTimeout(this.autoTimeout);
+        this.autoTimeout = setTimeout(() => {
+            this.close();
+        }, this.timeout)
+    }
+}
+
+export interface UserObject {
+    _id: ObjectID;
+    name: string;
+    email: string;
+    authorization: Array<string>,
+    games: Array<string>,
+    address: string;
+    passwordhash: string;
+    lastAuth: string;
+    session?:string;
+}
+
+export interface SessionObject {
+    _id:ObjectID;
+    sessionToken:string;
+    expiration:number;
+    userToken:string;
+}
+
+export interface GameParams {
+    _id: ObjectID;
+    token: string;
+}
+
+
+const url = () => {
+    const {baseurl, password, username, db} = MongoDetails
+    return `mongodb://${username}:${password}@${baseurl}/${db}`
+}
