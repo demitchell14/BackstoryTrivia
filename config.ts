42766dd2cd83d09a64ec8aedce4ce60c9eab0a13
Devin <demitchell14@gmail.com>
2018-11-21 21:21:30 -0600

    added Database

--- config.ts
+++ config.ts
@@ -3,7 +3,21 @@ const AuthorizationString = "i'm technically authorized";
 
 
 const Config = {
-    AuthString: AuthorizationString
+    AuthString: AuthorizationString,
+
+};
+
+export const Encryption = {
+    saltRounts: 10,
+    pattern: /^\$2[ayb]\$.{56}$/
+};
+
+export const MongoDetails = {
+    baseurl: "ds029541.mlab.com:29541",
+    username: "trivia",
+    password: "trivia123",
+    db: "sockettrivia",
+    maxSavedSessions: 15,
 };
 
 export const AuthString = AuthorizationString;
