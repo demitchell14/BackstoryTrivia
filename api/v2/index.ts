83101fd78fcd5f14fbb3753ef4bc3477639154ce
Devin <demitchell14@gmail.com>
2018-12-12 22:31:43 -0600

    /api/v2/     - /user     - /team

--- api/v2/index.ts
+++ api/v2/index.ts
@@ -1,9 +1,21 @@
+
 import * as express from "express";
+import * as graphqlHTTP from "express-graphql";
+//import schema from "./SpaceXSchemaDemo";
 
-
+import user from "./user";
+import team from "./team";
+//import {A} from "./UserSchema.mongoose";
 const router = express.Router();
 
+router.use('/user', user);
+router.use('/team', team);
+
+
+//router.use('/graphql', graphqlHTTP({
+//    schema,
+//    graphiql: true
+//}));
 
 
 export default router;
-//module.exports = router;
