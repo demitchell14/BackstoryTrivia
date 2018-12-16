8bc51c7cd951867edced0622e68b79c22d5e845a
Devin <demitchell14@gmail.com>
2018-12-15 20:52:45 -0600

    start of v2 layout

---
+++ react/src/v2/routes/questions/Questions.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+class Questions extends React.Component {
+    public render() {
+        return (
+            <div className={"container"}>
+                <div>Hello</div>
+            </div>
+        );
+    }
+}
+
+export default Questions;
