d812ab5fcc8acaa9ec2b91870c2bc8618c6e8451
Devin <demitchell14@gmail.com>
2018-12-15 20:52:45 -0600

    start of v2 layout

---
+++ react/src/v2/routes/dashboard/Dashboard.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Dashboard extends React.Component {
+    public render() {
+        return (
+            <div>
+                Hello Finally...
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
