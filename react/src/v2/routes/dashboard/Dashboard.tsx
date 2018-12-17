4dc1382480629f05f2c8ed36954814c1ecd8994f
Devin <demitchell14@gmail.com>
2018-12-16 23:37:13 -0600

    unimportant

--- react/src/v2/routes/dashboard/Dashboard.tsx
+++ react/src/v2/routes/dashboard/Dashboard.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
+import {Link} from "react-router-dom";
 
 class Dashboard extends React.Component {
     public render() {
         return (
             <div>
-                Hello Finally...
+                <p>I'm the Dashboard!</p>
+                <p>Visit the <Link to={"/questions"}>Questions Page</Link> as its currently the only one available right now :(</p>
             </div>
         );
     }
