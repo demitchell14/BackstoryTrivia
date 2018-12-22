828ce8dcccda7111b26324387ed16f0faa9c4f4c
Devin <demitchell14@gmail.com>
2018-12-22 03:52:16 -0600

    updates to Question Creator

--- react/src/v2/routes/creators/question/QuestionCreator.tsx
+++ react/src/v2/routes/creators/question/QuestionCreator.tsx
@@ -1,48 +1,269 @@
 import * as React from "react";
 
 import "./style.css";
+import {ChangeEvent, ReactNode, SyntheticEvent} from "react";
+//import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import QuestionPointValue from "./pointvalue/QuestionPointValue";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+//import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+//import InputGroupComponent from "../../../../components/InputGroupComponent";
+//import TagInput from "../../../components/input/tags/TagInput";
 
 class QuestionCreator extends React.Component<QuestionCreatorProps, QuestionCreatorState> {
     public constructor(props) {
         super(props);
-        this.state = {} as QuestionCreatorState
+        this.state = {
+            // history: [0, 1, 2],
+             history: [],
+            stage: 0,
+            stages: [
+                {
+                    renderer: this.renderStageZero
+                },
+                {
+                    renderer: this.renderCreateFresh,
+                    next: 2
+                },
+                {
+                    renderer: this.renderQuestionType,
+                    // isFinal: true
+                }
+
+            ],
+            form: {},
+             /*form: {
+                 question: "Who is the president?",
+                 timeLimit: 30,
+                 points: 3,
+                 category: "Current Events"
+             },*/
+            errors: {},
+        } as QuestionCreatorState
     }
 
     componentDidMount(): void {
-        console.log("Creator Mounted");
+
     }
 
     public render() {
+        const {stages, stage} = this.state;
         return (
-            <div onClick={this.overlayClicked} className={"creator-container centered"}>
+            <div onClick={this.overlayClicked} className={"creator-container centered flex-column"}>
                 <div onClick={this.containerClicked} className={"creator-body"}>
                     <div className={"card-body"}>
-                        <h2>Question Creator</h2>
-                        <p>I'm a body</p>
+                        <h2 className={"mb-4"}>Question Creator</h2>
 
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras consequat maximus purus lacinia posuere. Vestibulum fermentum ex magna, vitae ullamcorper elit tempor sed. Ut ultrices, nunc vitae congue maximus, est tortor molestie nunc, non pulvinar risus dui sed orci. Etiam convallis lectus ac tempus congue. Vestibulum cursus, sem ut ullamcorper cursus, dolor enim pellentesque enim, sit amet malesuada mi tortor id odio. Donec non enim ultricies, rhoncus massa lacinia, sodales nibh. Sed luctus iaculis magna eu commodo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas bibendum tincidunt quam gravida ornare.</p>
+                        <TransitionGroup>
+                            {this.renderStage(stage)}
+                        </TransitionGroup>
 
-                        <p>Donec scelerisque augue nec est vehicula, aliquet scelerisque eros luctus. Sed venenatis fringilla ipsum ac blandit. Donec aliquet diam gravida nunc posuere pretium. Maecenas sapien ipsum, mollis vitae venenatis et, placerat sed libero. Donec vestibulum, lectus non dapibus eleifend, quam ante posuere nisi, et rhoncus est erat vel est. Sed mollis dolor tellus, nec pellentesque nulla porta sit amet. Nunc id elementum metus. Aenean at viverra neque, eu cursus nibh. Nunc vulputate sapien sem, nec semper neque aliquam a.</p>
+                    </div>
 
-                        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc nulla lorem, gravida a lacinia ut, fringilla in elit. In nisi lacus, sodales at mauris non, mattis porta felis. Phasellus dolor felis, faucibus vitae congue quis, cursus ac quam. Fusce sed dui velit. Nullam lobortis, ex a congue pharetra, mi tortor efficitur eros, non rhoncus neque erat id leo. Fusce rhoncus quam vel dolor imperdiet, id ultrices nulla pulvinar. Donec diam mi, lacinia vel nisi at, faucibus elementum ipsum. Sed nisl magna, luctus eget viverra sit amet, eleifend sit amet ex.</p>
+                </div>
 
-                        <p>Duis aliquam orci sit amet sapien porta, eu finibus erat imperdiet. Quisque sit amet consectetur ex. Fusce molestie tempus lectus sit amet ultrices. Integer viverra vestibulum porta. Pellentesque ut tellus elit. Sed nec eleifend erat. Curabitur tristique, dui eget bibendum porta, massa nisi egestas leo, sit amet accumsan nulla enim et leo. In in elit at mauris convallis luctus. In hac habitasse platea dictumst.</p>
-
-                        <p>Curabitur aliquet leo felis, nec accumsan urna pellentesque at. Mauris purus sem, bibendum nec risus lobortis, sagittis elementum lectus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam non dolor et turpis pulvinar semper. Nullam eu molestie purus. Phasellus tempus ullamcorper est, non aliquam est scelerisque id. Integer dignissim, nisl non eleifend vulputate, lectus purus consequat arcu, et venenatis sapien quam ac eros. Nam ut justo massa. Sed non felis in elit mollis tincidunt at vitae nisi. Phasellus aliquam elit a sem aliquet, eu ullamcorper sem accumsan. Aliquam sit amet nisi nec ipsum semper rhoncus quis non lectus. Nam sit amet ante arcu. Maecenas a accumsan massa, eget mollis velit. Ut tempor, lorem at aliquam laoreet, dui quam sodales odio, a ultricies erat nisl eget ligula.</p>
+                <div onClick={this.containerClicked} className={"creator-footer menu-open"}>
+                    <div className={"ml-auto"}>
+                        <button onClick={this.goBack} className={"btn btn-outline-warning m-2"}>Go Back</button>
+                        <button onClick={this.onContinue} data-next={stages[stage].next} disabled={typeof stages[stage].next === "undefined" && typeof stages[stage].isFinal === "undefined"} className={`btn btn-outline-success m-2 ${stages[stage].isFinal ? "px-5" : ""}`}>{stages[stage].isFinal ? "Create" : "Continue"}</button>
                     </div>
                 </div>
             </div>
         );
     }
 
+    public onContinue = (evt:SyntheticEvent) => {
+        const {stages, stage} = this.state;
+        const next = evt.currentTarget.getAttribute("data-next");
+        if (next && stages[next]) {
+            //const target = stages[next];
+            if (stages[stage].isFinal) {
+
+            } else {
+                this.pushStage(next)
+            }
+        }
+    }
+
+    private renderStage =(stage:number) => {
+        if (this.state.stages[stage]) {
+            const st = this.state.stages[stage];
+            return (
+                <CSSTransition key={stage} classNames={"stage-ani"} timeout={400}>
+                    {st.renderer()}
+                </CSSTransition>
+            )
+        }
+        return <p>NA</p>
+    }
+
+    private renderStageZero = () => {
+        return (
+            <div className={"row my-3 justify-content-around"}>
+                <div className={"col-md-3 d-flex justify-content-center p-2"}>
+                    <button onClick={this.onModeSelect} data-stage={1} className={"creator-type"}>
+                        <h5 className={"border-bottom"}>From Scratch</h5>
+                        <p className={"mb-0"}>Create a question from no predefined information.</p>
+                    </button>
+                </div>
+                <div className={"col-md-3 d-flex justify-content-center p-2"}>
+                    <button className={"creator-type"} disabled>
+                        <h5 className={"border-bottom"}>From A Question</h5>
+                        <p className={"mb-0"}>Search through a list of questions that have already been made and use it as a template.</p>
+                    </button>
+                </div>
+            </div>
+        )
+    }
+
+    private renderCreateFresh = () => {
+
+        const formChange = (evt:ChangeEvent|any) => {
+            evt.preventDefault();
+            const el = evt.target as HTMLInputElement;
+            sendChange(el);
+        };
+        const sendChange = (el:{type: string; name: string; value: any;}) => {
+            const {type, name, value} = el;
+            const {form} = this.state;
+            switch (type) {
+                case "number":
+                    form[name] = Number.parseInt(value);
+                    break;
+                default:
+                    form[name] = value;
+            }
+
+            this.setState({form});
+        }
+
+        const test = (evt:SyntheticEvent) => {
+            alert (evt.currentTarget)
+        }
+        return (
+            <div className={"d-flex flex-column my-3"}>
+                <form data-next="2" onSubmit={test} className={""}>
+                    <p className="has-dynamic-label">
+                        <textarea onChange={formChange} name={"question"} value={this.state.form.question} className="dynamic-label-input" placeholder="Enter your Question" required />
+                        <label htmlFor="dynamic-label-input">Enter your Question</label>
+                    </p>
+                    <p className="has-dynamic-label">
+                        <input onChange={formChange} name={"category"} value={this.state.form.category} type={"text"} className="dynamic-label-input" placeholder="Enter categories" required />
+                        <label htmlFor="dynamic-label-input">Enter categories</label>
+                    </p>
+
+                    <div className={"row"}>
+
+                        <div className={"col-md-6"}>
+                            <div>
+                                <label style={{fontSize: "1.5em"}}>Question Time Limit</label>
+                                <QuestionPointValue name={"timeLimit"}
+                                                    value={this.state.form.timeLimit} onChange={sendChange}
+                                                    multipliers={[
+                                                        {max:300, interval: 15},
+                                                    ]} multiplierText={"second"}/>
+                            </div>
+                        </div>
+                        <div className={"col-md-6"}>
+                            <div>
+                                <label style={{fontSize: "1.5em"}}>Question Point Value</label>
+                                <QuestionPointValue name={"points"}
+                                                    value={this.state.form.points} onChange={sendChange}
+                                                    multipliers={[
+                                                        {max:3, interval: 1},
+                                                        {max:5, interval: 2},
+                                                        {max:10, interval: 5},
+                                                        {max:100, interval: 10}
+                                                    ]} multiplierText={"point"}/>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+
+    private renderQuestionType = () => {
+        const {state} = this;
+        const {form} = state;
+        const {question, answer, answers} = form;
+        if (answers) {
+            // TODO
+        } else
+        if (answer) {
+
+        }
+
+        const updateQuestion = (evt:ChangeEvent) => {
+            const ele = evt.target as HTMLInputElement;
+            const value = ele.value;
+            if (this.state.form.answers) {
+                // TODO
+            } else {
+                const form = this.state.form;
+                form.answer = value;
+                this.setState({form});
+            }
+        }
+
+        console.log(this.state.form)
+
+        return (
+            <form>
+                <p className={"lead"}>{question}</p>
+
+                <div className={"fa-layers fa-fw py-3 d-inline-block"}>
+                    <FontAwesomeIcon size={"5x"} icon={["far", "ban"]} />
+                    <FontAwesomeIcon size={"3x"} transform={"right-4"} icon={["fal", "keyboard"]} />
+                </div>
+
+                <div className={"list-group"}>
+                    <div className={"list-group-item"}>
+                        <div className={"text-success"}>Correct</div>
+                        <input onChange={updateQuestion} value={answers ? answers[0].answer : answer} placeholder={"-- value here --"}  className={"form-plain"} type={"text"}/>
+                    </div>
+                    <div className={"list-group-item"}>
+                        <button>Add Another Answer</button>
+                    </div>
+                </div>
+            </form>
+        )
+    }
+
     
-    
+    private onModeSelect = (evt:SyntheticEvent) => {
+        const target = evt.currentTarget.getAttribute("data-stage");
+        if (target) {
+            this.pushStage(target);
+        }
+    };
+
     private containerClicked =(evt) => {
         evt.stopPropagation();
     }
     private overlayClicked =(evt) => {
         evt.preventDefault();
         this.props.history.goBack();
+    }
+
+    private goBack = (evt) => {
+        evt.preventDefault();
+        const history = this.state.history;
+        history.pop()
+        this.setState({
+            stage: history[history.length-1] || 0,
+            history: history || [0]
+        });
+    }
+
+    private pushStage = (target:string) => {
+        const t = Number.parseFloat(target);
+        const currentHist = this.state.history;
+        currentHist.push(t);
+        this.setState({
+            stage: t,
+            history: currentHist
+        });
     }
 }
 
@@ -57,7 +278,25 @@ interface QuestionCreatorProps {
 }
 
 interface QuestionCreatorState {
-
+    stage: number;
+    stages: Array<{
+        renderer: () => ReactNode;
+        isFinal?: boolean;
+        next?:number;
+    }>;
+    history: number[];
+    form: {
+        timeLimit?:number;
+        category?: string;
+        question?: string;
+        points?:number;
+        answer?: string;
+        answers?: Array<{
+          answer: string;
+          correct: boolean;
+        }>;
+    }
+    errors: any;
 }
 
 export default QuestionCreator
