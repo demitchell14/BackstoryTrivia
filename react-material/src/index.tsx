cb9106d516ad32a3f305394cd585a48c8e7d46c8
Devin <demitchell14@gmail.com>
2019-01-27 01:34:35 -0600

    new FA Icons

--- react-material/src/index.tsx
+++ react-material/src/index.tsx
@@ -3,22 +3,39 @@ import * as ReactDOM from 'react-dom';
 
 import Main from "./Main";
 
-import { library} from "@fortawesome/fontawesome-svg-core";
+import {library} from "@fortawesome/fontawesome-svg-core";
 
-import {faHome, faPollH, faSlidersVSquare, faSignOutAlt, faChartNetwork, faClipboardListCheck} from "@fortawesome/pro-light-svg-icons";
+import {
+    faArrowFromRight,
+    faChartNetwork,
+    faClipboardListCheck,
+    faClone,
+    faCopy,
+    faEdit,
+    faEye,
+    faFile,
+    faFolderOpen,
+    faHome,
+    faPollH,
+    faSignOutAlt,
+    faSlidersVSquare,
+    faTimes,
+    faTrash,
+    faUnlink
+} from "@fortawesome/pro-light-svg-icons";
+import {faArrowLeft, faCheck, faHome as farHome, faImage, faMinus, faPlus} from "@fortawesome/pro-regular-svg-icons"
+import {faCheckCircle, faPlusCircle, faTimesCircle} from "@fortawesome/pro-solid-svg-icons"
+
 library.add(faHome, faPollH, faSlidersVSquare, faSignOutAlt, faChartNetwork, faClipboardListCheck);
 
-import {faTrash, faCopy, faClone, faFolderOpen, faEye, faFile, faTimes, faUnlink, faEdit} from "@fortawesome/pro-light-svg-icons";
 library.add(faTrash, faCopy, faClone, faFolderOpen, faEye, faFile, faTimes, faUnlink, faEdit);
 
-import {faImage} from "@fortawesome/pro-regular-svg-icons"
-library.add(faImage)
+library.add(faImage, farHome, faCheck, faPlus, faMinus, faArrowLeft, faArrowFromRight);
 
-import {faCheckCircle, faTimesCircle} from "@fortawesome/pro-solid-svg-icons"
-library.add(faCheckCircle, faTimesCircle)
+library.add(faCheckCircle, faTimesCircle, faPlusCircle);
 
 //import {} from "@fortawesome/pro-regular-svg-icons";
 //import {} from "@fortawesome/pro-solid-svg-icons";
 
 
-ReactDOM.render(<Main />, document.getElementById("root"))
+ReactDOM.render(<Main/>, document.getElementById("root"));
