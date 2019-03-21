ce88db34d1816fbea89a5fd8fc2238105f0330b9
Devin <demitchell14@gmail.com>
2019-03-20 21:10:07 -0500

    Beginning of Socket.IO connection / authentication

--- api/v2/index.ts
+++ api/v2/index.ts
@@ -7,9 +7,10 @@ import user from "./user";
 import team from "./team";
 import question from "./question";
 import games from "./games";
-import {Database, UserObject} from "../../util/db/DatabaseHandler";
+import {Database, GameObject, TeamObject, UserObject} from "../../util/db/DatabaseHandler";
 import {AuthorizedToken} from "../../util/jwt";
 import {ObjectID} from "bson";
+import {Cursor} from "mongodb";
 //import {A} from "./UserSchema.mongoose";
 const router = express.Router();
 
@@ -20,22 +21,34 @@ router.use('/games', games);
 
 export default router;
 
-export async function getUser(db:Database, jwt:AuthorizedToken):Promise<Partial<UserObject>|undefined>;
+export async function getUser(db:Database, jwt:AuthorizedToken):Promise<Partial<UserObject>|TeamObject>;
 export async function getUser(db:Database, jwt:AuthorizedToken) {
-    if (jwt.type === "moderator") {
-        await db.openCollection("moderators");
-        const file = await db.find({_id: ObjectID.createFromHexString(jwt._id)})
-        if (await file.count() === 1) {
-            const user = (await file.toArray())[0] as UserObject;
-            delete user.passwordhash;
-            delete user.pin;
-            delete user.savedUIDs;
-            delete user._id;
-            return user;
-        }
+
+    await db.openCollection(jwt.type + "s");
+    const file = await db.find({_id: ObjectID.createFromHexString(jwt._id)})
+    if (await file.count() === 1) {
+        const user = (await file.toArray())[0] as UserObject;
+        delete user.passwordhash;
+        delete user.pin;
+        delete user.savedUIDs;
+        delete user._id;
+        return user;
     }
-    if (jwt.type === "team") {
-        console.error("TODO");
+    return undefined;
+}
+
+export async function getGame(db:Database, token:string) {
+    await db.openCollection("games");
+    let cursor:Cursor<Object>;
+    if (ObjectID.isValid(token)) {
+        cursor = await db.find({_id: ObjectID.createFromHexString(token)})
+    } else {
+        cursor = await db.find({token});
     }
-    return {};
+
+    if (await cursor.count() === 1) {
+        const game = await cursor.next() as GameObject;
+        return game;
+    }
+    return undefined;
 }
