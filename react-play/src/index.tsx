a79b5d9ea96c33430c76d3bbed7af5fc85cfab48
Devin <demitchell14@gmail.com>
2019-03-21 00:11:28 -0500

    Added ReactGA Google Analytics

---
+++ react-play/src/index.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import * as  ReactDOM from 'react-dom';
+import * as ReactGA from "react-ga";
+
+import App from './App';
+
+import "normalize.css";
+import "./style/style.css";
+// import * as serviceWorker from './serviceWorker';
+
+const root = document.getElementById("root");
+
+ReactGA.initialize('UA-39450602-7', {
+    debug: true, //testMode: true,
+    
+});
+
+if (root) {
+    if (root.hasChildNodes()) {
+        ReactDOM.hydrate(<App />, root);
+    } else {
+        ReactDOM.render(<App />, root);
+    }
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    // Learn more about service workers: http://bit.ly/CRA-PWA
+    // serviceWorker.unregister();
+}
