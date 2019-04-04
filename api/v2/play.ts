7f6f78b1cbef7d65440fedaa5c365d43ecaa80da
Devin <demitchell14@gmail.com>
2019-04-04 00:17:17 -0500

    Fixed ObjectID key bugs in finding Teams and Questions added Game Status Route /api/v2/play/{game}/status added Game Question History Route /api/v2/play/{game}/history changed answers to be able to be changed up until the question time limit is over Question History now tracks your answers

--- api/v2/play.ts
+++ api/v2/play.ts
@@ -7,11 +7,11 @@ import jwt, {AuthorizedRequest} from "../../util/jwt";
 import {Database, UserObject} from "../../util/db/DatabaseHandler";
 import {ObjectID} from "bson";
 import {Cursor} from "mongodb";
-import {Mongoose,Document} from "mongoose";
+import {Mongoose, Document, Types} from "mongoose";
 import {Moderators} from "../../middleware/models/UserSchema";
 import GameInstanceManager from "../../util/GameInstanceManager";
 import SocketHandler from "../../util/SocketHandler.new";
-import {Game} from "../../notrivia";
+import {Answer, Game} from "../../notrivia";
 
 const router = express.Router();
 
@@ -62,9 +62,91 @@ class PlayRouting {
             }
         }
     }
+
+    getHistory = async (req:HistoryState, res:Response) => {
+        const {decoded} = req;
+        const userID = ObjectID.createFromHexString(decoded._id);
+        const {token} = req.params
+
+        const instanceManager = GameInstanceManager.getInstance(token)
+        const game = await instanceManager.instance()
+            .catch(err => res.status(500).json(err)) as Game;
+        if (game) {
+            if (decoded.type === "moderator") {
+                // TODO Handle differently
+                res.send("Moderators TODO");
+                return;
+            }
+
+            if (decoded.type === "team") {
+                if (game.hasTeam(userID)) {
+                    const answers = [] as Partial<Answer&{type: string}>[];
+                    const team = game.getTeam(userID);
+                    team.answers
+                       .sort((a, b) => Number(a.order < b.order))
+                       .map(ans => {
+                           answers.push({
+                               answer: ans.answer,
+                               question: ans.question,
+                               type: game.getQuestion(ans.question).type,
+                               _id: game.getQuestion(ans.question)._id,
+                           })
+                       })
+
+                    res.json({answers, token});
+                    return;
+                }
+            }
+
+            res.status(404).json({error: "You are not a part of this game."});
+        } else {
+            res.status(404).json({error: "Could not find game instance."});
+        }
+    }
+
+    getStatus = async (req:StatusRequest, res:Response) => {
+        const {decoded} = req;
+        const {token} = req.params
+
+        const instanceManager = GameInstanceManager.getInstance(token)
+        const game = await instanceManager.instance()
+            .catch(err => res.status(500).json(err)) as Game;
+        if (game) {
+            const response = {} as any;
+            if (decoded.type === "moderator") {
+                // TODO add extra properties since the user that requested status is a moderator ???
+            }
+
+            response.completed = game.getCurrentQuestionIndex() < 0;
+            response.started    = game.started;
+            response.paused     = game.paused;
+            // response.teams      = game.teams.length;
+            // response.questions  = game.questions.length;
+
+            if (response.completed) {
+                // TODO add extra status properties since the game is over ???
+            }
+
+            res.json(response);
+        } else {
+            res.status(404).json({error: "Could not find game instance."});
+        }
+    }
 }
 
 const routes = new PlayRouting();
+
+interface StatusRequest extends AuthorizedRequest, Middleware.Mongo {
+    params: {
+        token: string;
+    }
+}
+
+interface HistoryState extends AuthorizedRequest, Middleware.Mongo {
+    params: {
+        token: string;
+    }
+}
 
 interface IndexRoute extends AuthorizedRequest, Middleware.Mongo {
 
@@ -78,6 +160,8 @@ interface SendRoute extends AuthorizedRequest, Middleware.Mongo {
 
 
 router.get("/", jwt.authorized, routes.index);
+router.get("/:token/status", jwt.authorized, Mongo(), routes.getStatus);
+router.get("/:token/history", jwt.authorized, Mongo(), routes.getHistory);
 router.post("/send", jwt.authorized, routes.send);
 
 
