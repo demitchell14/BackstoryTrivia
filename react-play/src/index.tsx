854064598eb3282b14c490eeb810b424f31a72a5
Devin <demitchell14@gmail.com>
2019-04-11 20:34:46 -0500

    unregistered service worker for now

--- react-play/src/index.tsx
+++ react-play/src/index.tsx
@@ -26,5 +26,5 @@ if (root) {
     // If you want your app to work offline and load faster, you can change
     // unregister() to register() below. Note this comes with some pitfalls.
     // Learn more about service workers: http://bit.ly/CRA-PWA
-    serviceWorker.register();
+    serviceWorker.unregister();
 }
